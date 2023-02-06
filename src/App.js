f29613bc3d40f644cf6f71553dd3d225ac39a342
Larosh Benny <57269077+typeerror101@users.noreply.github.com>
2023-02-07 01:29:18 +0530

    Day-3

--- src/App.js
+++ src/App.js
@@ -2,29 +2,20 @@ import NavBar from './NavBar';
 import Pricing from "./pages/Pricing";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import { Route, Routes } from 'react-router-dom'
 
 export default function App() {
-  let component
 
-
-  switch(window.location.pathname){
-    case "/":
-      component = <Home/>
-      break
-    case "/pricing":
-      component = <Pricing/>
-      break
-    case "/about":
-      component = <About/>
-      break
-    default :
-  }
-  
   return (
     <>
     <NavBar/>
     <div className='component'>
-    {component}
+    <Routes>
+      <Route path='/' element={<Home/>} />
+      <Route path='/pricing' element={<Pricing/>} />
+      <Route path='/about' element={<About/>} />
+    </Routes>
+    
     </div>
     </>
   )
--- src/NavBar.js
+++ src/NavBar.js
@@ -1,19 +1,25 @@
-import React from 'react'
-
 export default function NavBar() {
   return (
     <div>
       <nav className='nav'>
         <a href="/" className='site-title'>Site Name</a>
         <ul>
-            <li className='active'>
-                <a href="/pricing">Pricing</a>
-            </li>
-            <li>
-                <a href="/about">About</a>
-            </li>
+            <CustomLink href="/pricing">Pricing</CustomLink>
+            <CustomLink href="/about">About</CustomLink>
         </ul>
       </nav>
     </div>
+  )
+}
+
+function CustomLink({href, children, ...props}){
+  const path = window.location.pathname
+
+  return(
+    <li className={path === href ? "active" : ""}>
+        <a href={href} {...props}>
+        {children}
+        </a>
+    </li>
   )
 }
